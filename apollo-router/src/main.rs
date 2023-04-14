ee9df4e1d1eb2d90321970148c5a33fd27a50cf1
Gary Pennington <gary@apollographql.com>
2023-04-14 13:48:23 +0100

    Can we measure information about wasted memory?

--- apollo-router/src/main.rs
+++ apollo-router/src/main.rs
@@ -1,6 +1,12 @@
 //! Main entry point for CLI command to start server.
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    unsafe {
+        let info = libc::mallinfo2();
+        tracing::info!("keepcost: {}", info.keepcost);
+    }
+
     match apollo_router::main() {
         Ok(_) => {}
         Err(e) => {
